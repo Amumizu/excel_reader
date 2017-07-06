e317d1c87ed8008fab9897b603d2ba673a710312
Amumizu <hohoho312@gmail.com>
2017-07-06 16:55:09 -0500

    Add files via upload

--- Analysis.cs
+++ Analysis.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Data;
+using System.Xml;
 
 /// <summary>
 /// The main class that processes all information
 /// </summary>
 /// 
-namespace XMLAnalysis{
-public class Analysis
+namespace XMLAnalysis
 {
-    /// <summary>
-    /// Takes in a xml file in order to check that there are not multiple columns in the same row with data, as well as checking that 
-    /// the only data inputted is '1'.
-    /// </summary>
-    /// <param name="table"></param>
-    /// The data in an xml file as a string
-    /// <returns></returns>
-    public string validateTable(string table)
+    public class Analysis
     {
+        /// <summary>
+        /// Takes in a xml file in order to check that there are not multiple columns in the same row with data, as well as checking that 
+        /// the only data inputted is '1'.
+        /// </summary>
+        /// <param name="table"></param>
+        /// The data in an xml file as a string
+        /// <returns></returns>
+        public string validateTable(XmlDocument data)
+        {
 
+        }
     }
-}
 }
